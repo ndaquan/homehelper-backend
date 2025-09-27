46d3f2769b07d5a072a23864a7036a4a95a67e53
KhanhNguyen03 <khanhnguyn2528@gmail.com>
2025-09-28 01:40:48 +0700

    Wishlist_Tasker

--- server.js
+++ server.js
@@ -77,6 +77,8 @@ app.use("/api/tasker-profile", require("./routes/taskerProfile"));
 app.use("/api/bookings", require("./routes/bookings"));
 // app.use('/api/posts', require('./routes/posts'));
 app.use("/api/ratings", require("./routes/ratings"));
+app.use("/api/wishlists", require("./routes/wishlist"));
+
 // app.use('/api/feedbacks', require('./routes/feedbacks'));
 // app.use('/api/complaints', require('./routes/complaints'));
 // app.use('/api/notifications', require('./routes/notifications'));
@@ -96,7 +98,6 @@ app.use("/api/videos", require("./routes/videoRoutes"));
 // app.use('/api/users', require('./routes/users'));
 // app.use('/api/bookings', require('./routes/bookings'));
 // app.use('/api/posts', require('./routes/posts'));
-// app.use('/api/ratings', require('./routes/ratings'));
 
 // Health check endpoint
 app.get("/health", (req, res) => {
